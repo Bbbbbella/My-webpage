11b8a42370448627266f91fd0a98718892e89e7c
Bbbbbella <56407654+Bbbbbella@users.noreply.github.com>
2019-10-17 15:13:13 -0400

    Update myJavaScrip.js

--- myJavaScrip.js
+++ myJavaScrip.js
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html>
-<body>
-<head>
- <style>
-  #demo {
-  background-color: #f44336;
-  }
- </style>
-</head>
-<h1>The onclick Event</h1>
 
-<script>
 document.getElementById("myButton").addEventListener("click", myFunction);
 
 function myFunction() {
   document.getElementById("myButton").innerHTML = "Bella";
 }
-</script>
-
-</body>
-</html>
